fd1291163a9eab50301bef8b34d2014a51d1b75c
nwunderly <48489521+nwunderly@users.noreply.github.com>
2020-11-03 18:01:29 -0500

    fix requirements.txt reference

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ RUN mkdir /squire
 
 WORKDIR /squire
 
+COPY requirements.txt requirements.txt
+
 RUN pip3 install -r requirements.txt
 
 COPY . .
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 git pull
 docker build . --tag squire:latest
-docker run -d \
+docker run \
  --name squire \
  -v $PWD/logs:/squire/logs \
  -v $PWD/data:/squire/data \
