c3c2bffd3bcd7b218c94103f8c505b0aa7a0c1c7
nwunderly <48489521+nwunderly@users.noreply.github.com>
2020-11-07 15:52:55 -0500

    new network

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@ git pull
 docker build . --tag squire:latest
 docker run -d \
  --name squire \
+ --network prod \
  -v $PWD/logs:/squire/logs \
  -v $PWD/data:/squire/data \
  --restart unless-stopped \
- --network bots \
  squire
