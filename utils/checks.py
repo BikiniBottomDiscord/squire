3743ea730d2c0a2ecb6457ee057740e0233d36e7
nwunderly <48489521+nwunderly@users.noreply.github.com>
2021-01-02 21:26:23 -0500

    bump version

--- utils/checks.py
+++ utils/checks.py
@@ -2,30 +2,10 @@ import logging
 
 from discord.ext import commands
 
+from utils.settings import MODS, ADMINS
+
 
 logger = logging.getLogger('utils.checks')
-
-ADMINS = {
-    204414611578028034,  # rev
-    304695409031512064,  # dove
-    426550338141683713,  # dee
-    224323277370294275,  # kiwi
-    298497141490450432,  # swine
-    448250281097035777,  # sonic
-}
-
-MODS = {
-    204414611578028034,  # nwunder#0003
-    279722793891790848,  # Moistley#5939
-    533087803261714433,  # Neptune's Helper#0039
-    375375057138089986,  # Saturnfive050#1337
-    304695409031512064,  # dovedevic#0522
-    280874216310439938,  # egg#2222
-    448250281097035777,  # No Jons#1299
-    224323277370294275,  # Kiwi#6666
-    299023554127593473,  # mzone#1771
-    298497141490450432,  # AdventurousSwine#9894
-}
 
 
 def is_admin(user):
