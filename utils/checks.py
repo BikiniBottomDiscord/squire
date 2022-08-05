469d5b1009ae862128d5cee6f92e40da1b3ae9b6
Nate <nwunder.dev@gmail.com>
2022-08-05 18:58:21 -0400

    Update checks.py

--- utils/checks.py
+++ utils/checks.py
@@ -7,12 +7,19 @@ from utils.settings import ADMINS, MODS
 logger = logging.getLogger("utils.checks")
 
 
+def _has_role(user, roleid):
+    for role in user.roles:
+        if role.id == roleid:
+            return True
+    return False
+
+
 def is_admin(user):
     return user.id in ADMINS
 
 
 def is_mod(user):
-    return user.id in MODS
+    return is_admin(user) or _has_role(user, 736363032304943135)
 
 
 def hall_monitor():
